130497b198718611f4430cb73aa2c1889431bbaf
Viorel Stirbu <viorels@gmail.com>
2013-09-09 20:21:19 +0300

    jinja2 temlates

--- messages-app.py
+++ messages-app.py
@@ -2,13 +2,19 @@ import cgi
 import logging
 import os
 import webapp2
+import jinja2
 from google.appengine.api import users
-from google.appengine.ext.webapp import template
 
 from models import Message
 
 # Set to true if we want to have our webapp print stack traces, etc
 _DEBUG = True
+
+app_path = os.path.dirname(__file__)
+templates_path = os.path.join(app_path, 'templates')
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(templates_path),
+    extensions=['jinja2.ext.autoescape'])
 
 
 class BaseRequestHandler(webapp2.RequestHandler):
@@ -23,33 +29,26 @@ class BaseRequestHandler(webapp2.RequestHandler):
             'request': self.request,
             'user': users.get_current_user(),
             'logout_url': users.create_logout_url(self.request.uri),
-            'application_name': 'Udacity messages',
+            'application_name': 'Udacity Messages',
         }
         values.update(template_values)
-        directory = os.path.dirname(__file__)
-        path = os.path.join(directory, os.path.join('templates', template_name))
-        self.response.out.write(template.render(path, values, debug=_DEBUG))
+        template = JINJA_ENVIRONMENT.get_template(template_name)
+        self.response.out.write(template.render(values))
 
 
 class MainPage(BaseRequestHandler):
     def get(self):
         user = users.get_current_user()
         if user:
-            self.render('base.html')
+            self.render('home.html')
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
 
-class MessagesPage(BaseRequestHandler):
+class InboxPage(BaseRequestHandler):
     def get(self):
         user = users.get_current_user()
-        self.response.headers['Content-Type'] = 'text/html'
-        text = 'Hello, %s<br>' % user.nickname()
-        self.response.write(text)
-
-        for message in Message.list_for_user(user):
-            message_info = "%s (%s)" % (message.subject, message.from_user)
-            self.response.write(cgi.escape(message_info) + '<br>')
+        self.render("inbox.html", {'messages': Message.list_for_user(user.email())})
 
 
 class ComposePage(BaseRequestHandler):
@@ -76,6 +75,7 @@ class InitPage(BaseRequestHandler):
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/messages', MessagesPage),
+    ('/inbox', InboxPage),
+    ('/compose', ComposePage),
     ('/init', InitPage),
 ], debug=_DEBUG)
